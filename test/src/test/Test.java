e0d40f41756a4ca89bb11f69a80259210fac7bd6
shimp <shimp@DESKTOP-R5NC918>
2017-12-26 14:41:34 +0900

    hello3

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.println("hello3");
 	}
 
 }
