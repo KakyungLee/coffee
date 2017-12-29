522668081b2734efd0c16eceed969ae0047bbaaf
DESKTOP-DRV2VE7\MOA <moa8520@naver.com>
2017-12-29 17:10:18 +0900

    UI기본

--- pos_coffee/src/pos_coffee/TabPanel.java
+++ pos_coffee/src/pos_coffee/TabPanel.java
@@ -4,6 +4,32 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+
+import java.awt.*;
+import javax.swing.*;
+
+public class TabPanel extends JPanel{
+	//상단 탭 패널
+	
+	protected JButton mainBtn = new JButton("메인");	//메인패널로 이동
+	protected JButton productBtn = new JButton("상품관리");	//상품관리 패널로 이동
+	protected JButton memberBtn = new JButton("회원관리");	//회원관리 패널로 이동
+	protected JButton salesBtn = new JButton("매출관리");	//매출관리 패널로 이동
+	protected JLabel dataClockLbl = new JLabel("시계");	//날짜와 시간 표시
+	
+	TabPanel(){
+		setBackground(Color.lightGray);
+		add(mainBtn);
+		add(productBtn);
+		add(memberBtn);
+		add(salesBtn);
+		add(dataClockLbl);
+	}
+	
+}
+
+
+/*
 public class TabPanel extends JFrame {
 	protected JPanel Bottom;
 	// 상단 부분
@@ -71,3 +97,5 @@ public class TabPanel extends JFrame {
 		salesBtn.addActionListener(listener);
 		}
 }
+
+*/
